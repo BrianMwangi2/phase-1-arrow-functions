cf74eb263bbd9ce6979e7225ddc1998e7eb3faf0
Brian <brian.mwangi2@student.moringaschool.com>
2024-03-25 15:15:06 +0300

    creates arrows

---
+++ index.js
@@ -0,0 +1,19 @@
+//division
+const divide=(a,b)=>{
+    return a/b
+}
+divide(2000,100);
+//squares     
+const square=(x)=>{
+    return x**x
+}
+square(2);
+
+
+// Add
+const add=(a,b)=>{
+    console.log(`adding${a}`)
+    console.log(`adding${b}`)
+    return  a+b
+}
+add(3,4)
